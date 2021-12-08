61f41923c3d3b726b765f59c5d856a58fc96267f
AlexandreOkita <xande.okita@gmail.com>
2021-12-08 20:11:06 -0300

    add input

--- mutex_client.py
+++ mutex_client.py
@@ -4,6 +4,9 @@ if __name__ == '__main__':
     with open("/home/ubuntu/mc714/mutex_server_ip") as f:
             server = f.readlines()[0].strip()
     print("Sending to IP:", server)
-    client = c.UnaryClient(server, 5000)
-    response = client.send_message('banana')
-    print(response)
+    message = input("Enter message: ")
+    while message != "" or message != "exit":
+        client = c.UnaryClient(server, 5000)
+        response = client.send_message(message)
+        print(response)
+        print()

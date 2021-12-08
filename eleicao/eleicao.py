1e15d68ff1c5979291b6c44e26a5dd67e5795ca1
AlexandreOkita <xande.okita@gmail.com>
2021-12-08 17:17:46 -0300

    stop using localhost

--- eleicao/eleicao.py
+++ eleicao/eleicao.py
@@ -6,6 +6,7 @@ import time
 import test_pb2_grpc as pb2_grpc
 import test_pb2 as pb2
 import os
+import urllib.request
 
 class UnaryService(pb2_grpc.UnaryServicer):
 
@@ -23,17 +24,15 @@ class UnaryService(pb2_grpc.UnaryServicer):
             num = message.split(",")[1]
             self.process.eleicao_ocorrendo = True
             if num < self.process.my_number:
-                #! Enviar mensagem ao succ ao invés de response
-                #! Descobrir como verificar o próximo nó DISPONÍVEL
                 self.process.send_message_to_next(f"eleicao,{self.process.my_number}")
                 return pb2.MessageResponse(**{'message': f"continuado", "received": True})
             if num > self.process.my_number:
                 self.process.send_message_to_next(f"eleicao,{num}")
                 return pb2.MessageResponse(**{'message': f"continuado", "received": True})
             self.process.coordenador = self.process.my_number
-            for port in self.process.other_numbers:
+            for address in self.process.other_numbers:
                 try:
-                    c.UnaryClient('localhost', port).send_message(f"OK,{self.process.my_number},{port}")
+                    c.UnaryClient(address, '5000').send_message(f"OK,{self.process.my_number},{address}")
                 except:
                     continue
             self.process.eleicao_ocorrendo = False
@@ -48,27 +47,28 @@ class UnaryService(pb2_grpc.UnaryServicer):
         
 class Process:
     def __init__(self):
-        self.coordenador = "5432"
-        self.my_number = os.environ['ELECTION_PORT']
+        self.my_number = urllib.request.urlopen('https://ident.me').read().decode('utf8')
         self.other_numbers = []
-        self.eleicao_ocorrendo = False
+        self.coordenador = ""
+        self.eleicao_ocorrendo = False        
 
     def get_other_processes(self):
         with open("/home/ubuntu/mc714/hosts") as f:
             all_numbers = [line.strip() for line in f.readlines()]
+        self.coordenador = max(all_numbers)
         self.other_numbers = (all_numbers[all_numbers.index(self.my_number):]+all_numbers[:all_numbers.index(self.my_number)])[1:]
     
     def send_message_to_next(self, message):
         for succ in self.other_numbers:
             try:
                 print("Trying to send message to", succ)
-                return c.UnaryClient('localhost', succ).send_message(message)
+                return c.UnaryClient(succ, '5000').send_message(message)
             except:
                 time.sleep(2)
         print("no other node to send message")
     
     def send_message_to_coordinator(self, message):
-        return c.UnaryClient('localhost', self.coordenador).send_message(message)
+        return c.UnaryClient(self.coordenador, '5000').send_message(message)
 
 
 def serve(port, process):
@@ -82,6 +82,8 @@ def serve(port, process):
 if __name__ == '__main__':
     process = Process()
     process.get_other_processes()
+    print("My number:", process.my_number)
+    print("Current coordinator:", process.coordenador)
 
     # Recebe mensagem de eleição
     Thread(target=serve, args=(process.my_number, process,)).start()
--- mutex_client.py
+++ mutex_client.py
@@ -3,6 +3,7 @@ import client as c
 if __name__ == '__main__':
     with open("/home/ubuntu/mc714/mutex_server_ip") as f:
             server = f.readlines()[0].strip()
-    client = c.UnaryClient('server', 5000)
+    print("Sending to IP:", server)
+    client = c.UnaryClient(server, 5000)
     response = client.send_message('banana')
     print(response)
